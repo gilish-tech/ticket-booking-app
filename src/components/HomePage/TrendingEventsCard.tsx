e34b7fc0a5c37109630f7b14955e78fc1568d815
GILFROSH <smartking3077@gmail.com>
2024-09-22 00:31:26 +0100

    lazy load image

--- src/components/HomePage/TrendingEventsCard.tsx
+++ src/components/HomePage/TrendingEventsCard.tsx
@@ -1,6 +1,8 @@
 import { LuArrowUpRight } from "react-icons/lu";
 import { formatDate } from "../../lib/utils";
 import {Link} from "react-router-dom"
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 type TrendingEventsCardProps = {
     image:string,
     title:string,
@@ -15,7 +17,10 @@ type TrendingEventsCardProps = {
 const TrendingEventsCard = ({image,date,title,description,time,id}:TrendingEventsCardProps) => {
   return (
     <div className="overflow-hidden shadow-lg py-2 rounded-md">
-            <img loading="lazy" className="w-full max-h-[200px] rounded-md"  src={image} alt="Sunset in the mountains" />
+            {/* <img loading="lazy" className="w-full max-h-[200px] rounded-md"  src={image} alt="Sunset in the mountains" /> */}
+            <LazyLoadImage  effect="blur" wrapperProps={{
+        // If you need to, you can tweak the effect transition using the wrapper style.
+                style: {transitionDelay: "1s"},}} loading="lazy" wrapperClassName="w-full bg-gray-200"  className="w-full max-h-[200px] rounded-md bg-gray-200"  src={image} alt="Sunset in the mountains" />
             <div className="px-6 py-4">
                 <div className="font-semibold mb-1">
                     <h3 className="text-lg md:text-xl text-gray-800">{title}</h3>
